64989056e97cb1a86742934807247ab97b3611ae
johnverz77 <johnverz77@gmail.com>
2024-10-21 16:33:08 +0800

    Initial commit

---
+++ src/main/java/org/infoman2/DatabaseConnection.java
@@ -0,0 +1,24 @@
+package org.infoman2;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DatabaseConnection {
+    private String url = "jdbc:mysql://localhost:3340/fxapp";
+    private String user = "app";
+    private String password = "1234";
+    private Connection connection;
+
+    public DatabaseConnection(){
+        try{
+            connection = DriverManager.getConnection(url, user, password);
+            System.out.println("Connected successfully");
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public Connection getConnection(){
+        return connection;
+    }
+}
---
+++ src/main/java/org/infoman2/Main.java
@@ -0,0 +1,7 @@
+package org.infoman2;
+
+public class Main {
+    public static void main(String[] args) {
+        DatabaseConnection db = new DatabaseConnection();
+    }
+}
